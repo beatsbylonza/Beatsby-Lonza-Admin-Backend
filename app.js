9ad95431faa89eb6092aa90e9a387831fa1e68e0
ecoknows <jerico.villaraza@gmail.com>
2022-03-20 00:40:38 +0800

    :construction: setup database in progress

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
-var registerRouter = require('./routes/register');
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 /* Register API */
 router.post('/',async function(req, res, next){
-
+	const body = req.body;
+	
+	await User.create({
+		...body,
+		password: bcrypt.hash(body.password, 8)
+	});
 });
 
 module.exports = router;
