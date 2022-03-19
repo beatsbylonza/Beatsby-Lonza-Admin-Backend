3a3a167f5b834a746b02dbc383bba4a58e785db1
ecoknows <jerico.villaraza@gmail.com>
2022-03-20 00:17:27 +0800

    :sparkles: First commit

---
+++ routes/login.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* Login Functions */
+function authenticate(name, pass, fn){
+	
+}
+
+/* Login API */
+router.post('/', async function(req, res, next){
+	
+});
+
+module.exports = router;
